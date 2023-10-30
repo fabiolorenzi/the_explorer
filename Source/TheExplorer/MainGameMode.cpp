01f00036bf541e2c1daeda1321a3a1f35e364b25
fabiolorenzi <fabiolorenzi13975@gmail.com>
2023-10-30 15:29:58 +0000

    developed basic main game mode class

---
+++ Source/TheExplorer/MainGameMode.cpp
@@ -0,0 +1,13 @@
+#include "MainGameMode.h"
+
+#include "GameFramework/Actor.h"
+
+AMainGameMode::AMainGameMode()
+{
+    PrimaryActorTick.bCanEverTick = true;
+}
+
+void AMainGameMode::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
